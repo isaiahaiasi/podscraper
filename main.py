27e7e7aa31f725255feb79ee9e01d5ce5815910e
Isaiah <computerblender@gmail.com>
2021-12-03 16:57:35 -0700

    Implement URL generator with leading-00s fn

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import os
+import math
+
 from dotenv import load_dotenv
 load_dotenv()
 
 
-print(os.environ.get('ENV_VAR'))
+# only for 0+
+def pad_num(n, len=3):
+    if n == 0:
+        return '0'*len
+
+    digits = int(math.log10(n))+1
+    return '0'*(len-digits) + str(n)
+
+
+def get_urls():
+    return [os.environ.get('POD_URL') + '/' + pad_num(i) for i in range(491)]
+
+
+print(get_urls())
